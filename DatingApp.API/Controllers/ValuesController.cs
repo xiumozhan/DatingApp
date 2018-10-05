d33e8a8580a9ece0cfe194a20dc0ed32f47f2fcd
Xiumo Zhan <xiumozhan@gmail.com>
2018-10-04 23:56:33 -0600

    Client authorization, error handling and styling

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace DatingApp.API.Controllers
             this.context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public  async Task<IActionResult> GetValues()
         {
